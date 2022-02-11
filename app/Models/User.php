8c8075831f7002eed080ead07bb3e2b98d5b8ac6
s <99058368@mydavinci.nl>
2022-02-11 11:53:35 +0100

    remove user timestamp add rank fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +20,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'role',
         'password',
     ];
 
