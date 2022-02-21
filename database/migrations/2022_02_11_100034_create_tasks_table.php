0e2ab577f1f9cde9aeb4149a371871d412a8b287
s <99058368@mydavinci.nl>
2022-02-21 22:17:51 +0100

    remove options table and add single json column to tasks.

--- database/migrations/2022_02_11_100034_create_tasks_table.php
+++ database/migrations/2022_02_11_100034_create_tasks_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('table_id');
             $table->string('task_name');
+            $table->json('items')->nullable();;
         });
     }
 
