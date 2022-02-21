c4cfefe91db8a28a85ee53045e0faf1b351eaa83
s <99058368@mydavinci.nl>
2022-02-21 10:04:49 +0100

    make editable

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'user_id',
+        'table_name',
+        'table_creation',
+    ];
 }
