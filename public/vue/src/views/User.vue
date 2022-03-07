724c1fc6870a6240f0dd82d6bc2b50925b51c7d4
s <99058368@mydavinci.nl>
2022-03-07 11:36:15 +0100

    dialog to add option

--- public/vue/src/views/User.vue
+++ public/vue/src/views/User.vue
@@ -18,7 +18,7 @@
          </v-col>
        </v-row>
        <v-row>
-            <v-col v-for="list in todos" :key="list.id" sm="12" lg="4" md="4">
+            <v-col v-for="list in todos" :key="list.id" sm="12" lg="4" md="6">
                 <v-card>
                     <v-card-title>
                        {{list.table_name}}
@@ -52,14 +52,16 @@
                                 </v-list-item-content>
                             </v-list-item>
                         </v-list-item-group>
-
                     </v-list>
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn class="red--text" text>
                             Verwijderen
                         </v-btn>
-                        <v-btn @click="loadItems(list.table_id)" class="green--text" text>
+                        <v-btn text @click="add_row(list.id, 'open_menu')" color="primary">
+                            Nieuw taak
+                        </v-btn>
+                        <v-btn v-if="list.task_name[0] !== null" @click="loadItems(list.table_id)" class="green--text" text>
                             Bekijken
                         </v-btn>
                     </v-card-actions>
@@ -70,35 +72,75 @@
         <v-dialog dark v-model="selected_dialog" width="1000">
             <v-sheet elevation="6">
                 <v-tabs  background-color="teal darken-5" dark
-                    next-icon="mdi-arrow-right-bold-box-outline"
-                    prev-icon="mdi-arrow-left-bold-box-outline"
-                    show-arrows>
+                         next-icon="mdi-arrow-right-bold-box-outline"
+                         prev-icon="mdi-arrow-left-bold-box-outline"
+                         show-arrows>
                     <v-tabs-slider color="white"></v-tabs-slider>
-<!--                    TABS SELECTED ITEMS-->
+                    <!--                    TABS SELECTED ITEMS-->
                     <v-tab v-for="list in selected" :key="list.id" :href="'#tab-'+list.id">
                         {{list.task_name}}
                     </v-tab>
-<!--                    ITEMS SELECTED ITEMS-->
-                    <v-tab-item class="ma-2" v-for="list in selected" :key="list.id" :value="'tab-'+list.id">
-                        <v-card>
-                            <v-row class="ma-2 text-center">
-                                <v-col cols="4">
-<!--                                    name-->
+                    <!--                    ITEMS SELECTED ITEMS-->
+                    <v-tab-item class="ma-2" v-for="(list) in selected" :key="list.id" :value="'tab-'+list.id">
+                        <v-card flat v-for="item in list.items" :key="item.id">
+                            <v-row id="properties" @click="run_Click(item.id)" class="ma-2 text--black">
+                                <v-col v-if="item.status && item.status !== 'archived'" class="green lighten-4" cols="12">
+                                    <!--                                    name-->
+                                    <h4>{{item.name}}</h4>
                                 </v-col>
-                                <v-col cols="4">
-<!--                                    status-->
+                                <v-col v-if="!item.status && item.status !== 'archived'" class="red lighten-4" cols="12">
+                                    <!--                                    name-->
+                                    <h4>{{item.name}}</h4>
                                 </v-col>
-                                <v-col cols="4">
-<!--                                    labels-->
+                                <v-col v-if="item.status === 'archived'" class="grey lighten-2" cols="12">
+                                    <!--                                    name-->
+                                    <h4 class="text-decoration-line-through font-weight-light">{{item.name}}</h4>
                                 </v-col>
                             </v-row>
-                            {{list.items}}
                         </v-card>
                         <v-spacer></v-spacer>
+<!--                        <v-btn text @click="add_item = !add_item"  class="mt-2 float-end" color="primary">-->
+<!--                            Nieuwe taak toevoegen.-->
+<!--                        </v-btn>-->
                     </v-tab-item>
                 </v-tabs>
             </v-sheet>
         </v-dialog>
+
+
+        <v-dialog width="400" v-model="add_item">
+            <v-card>
+                <v-form ref="add_new_task">
+                    <v-card-title>
+                        Nieuwe item toevoegen
+                    </v-card-title>
+                    <v-card-text>
+                        <v-text-field
+                            v-model="create_option"
+                            :rules="addOptionInput"
+                            label="Name"
+                            required
+                        ></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            color="red"
+                            text
+                            @click="add_item = false"
+                        >
+                            Sluiten
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn @click="add_row()"
+                               color="primary"
+                               text
+                        >Toevoegen
+                        </v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card>
+        </v-dialog>
+
     </v-container>
     </v-app>
 </template>
@@ -112,6 +154,13 @@ export default {
             todos: [],
             selected: [],
             selected_dialog: false,
+            add_item: false,
+            create_option: '',
+            addOptionInput: [
+                v => !!v || 'Veld is leeg',
+                v => v && v.length >= 3 || 'Minimaal 3 letters of cijfers',
+                v => /^[a-zA-Z0-9]+$/.test(v)||'Alleen letters of cijfers toegestaan',
+            ],
         }
     },
     methods: {
@@ -120,28 +169,47 @@ export default {
                this.selected_dialog = !this.selected_dialog;
            }
            this.$store.dispatch('grab_todos_tasks', id).then(response => {
+               response.forEach( item => item.items = JSON.parse(item.items))
                this.selected = response;
            })
         },
         loadData(){
             //New request
             this.$store.dispatch('grab_todos_list').then(response => {
+                console.log(response);
                 let data = [];
                 //Loop through returned data
                 response.forEach(function (item){
                     //Search if json object iteration exists
-                    let loop = data.find(element => element['table_id'] === item['table_id']);
+                    let loop = data.find(element => element['table_name'] === item['table_name']);
                     //Add item to existing iteration
                     if(loop){
                         loop.task_name.push(item.task_name);
                     } else {
                         //Create new iteration
-                        data.push({id: item['id'], table_id: item['table_id'], table_name: item['table_name'], username: item['username'], table_creation: item['table_creation'], task_name: [item['task_name']]})
+                        data.push({id: item['todo_id'], table_id: item['table_id'], table_name: item['table_name'], username: item['username'], table_creation: item['table_creation'], task_name: [item['task_name']]})
                     }
                 })
                 this.todos = data;
+                console.log(data);
                 //  Error
             })
+        },
+        add_row(index, action){
+            if(action){
+                this.selected = index;
+                return this.add_item = !this.add_item;
+            }
+            console.log(this.selected)
+            if(this.$refs.add_new_task.validate()) {
+               this.$store.dispatch('add_new_task', [this.selected, this.create_option]).then(() => {
+                    this.add_item = false;
+                    this.loadData();
+               })
+           }
+        },
+        run_Click(index){
+            console.log(index)
         }
     },
     mounted() {
@@ -151,5 +219,7 @@ export default {
 </script>
 
 <style scoped>
-
+#properties:hover{
+    cursor: pointer;
+}
 </style>
