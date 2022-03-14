288ff5145d1a7c42a8ccad3445fc8a226b2d58e1
s <99058368@mydavinci.nl>
2022-03-14 04:14:28 +0100

    edit to make option adding work

--- app/Http/Controllers/Consumer.php
+++ app/Http/Controllers/Consumer.php
@@ -80,7 +80,11 @@ class Consumer extends Controller
         return Todo::where([['user_id', '=', auth()->user()->id]])->leftJoin('tasks', 'tasks.table_id', '=', 'todos.id')->leftJoin('users', 'users.id', '=', 'todos.user_id')->get(['todos.table_name','tasks.table_id', 'tasks.task_name', 'todos.table_creation', 'users.name as username', 'tasks.id as id','todos.id as todo_id', 'users.id as username_id']);
     }
     public function user_grab_tasks(Request $request){
-        return Todo::where([['user_id', '=', auth()->user()->id], ['table_id', '=', $request->id]])->join('tasks', 'tasks.table_id', '=', 'todos.id')->join('options', 'options.task_id', '=', 'tasks.id')->get(['tasks.tasK_name', 'options.*']);
+        $task = Task::where('table_id', $request->id);
+        return $task->with('options')->get();
+    }
+    public function user_grab_single_item(Request $request){
+        return Option::where([['id', '=', $request->id]])->get();
     }
 
     public function user_add_task(Request $request){
@@ -103,14 +107,14 @@ class Consumer extends Controller
     }
 
     public function user_add_item(Request $request){
-        return Option::where([['task_id', '=', $request->id]])->get();
-//        $items = $items->reject(null);
-//        $object = (object)[
-//            'id' => 5,
-//            'name' => $request->name,
-//            'status' => true,
-//            'tasks' => [],
-//            'label' => []
-//        ];
+        $new = new Option;
+        $new->name = $request->list;
+        $new->status = 1;
+        $new->tasks = true;
+        $new->label = true;
+        $new->task_id = $request->id;
+        $new->save();
+
+
     }
 }
